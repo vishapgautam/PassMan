5f8b890749a0952efe91709d863cc33f140ad9fc
vishapgautam <1900560310028@bbdniit.ac.in>
2022-03-01 00:50:17 +0530

    first commit

--- controllers/passwordsController.js
+++ controllers/passwordsController.js
@@ -2,27 +2,36 @@ const Password=require('../models/passwords')
 const User=require("../models/users")
 const bcrypt=require("bcryptjs")
 
-module.exports.getPass=async(req,res)=>{
-  const emailId=req.body.emailId
-  const password=req.body.password
-  if (!emailId | !password) res.status(400).json({success:"false",description:"provide email and password"})
-     const user=await User.findOne({emailId:emailId})
-     if (!user) res.status(400).json({success:"false",description:"user dosen't exists"})
-     const confirm=await bcrypt.compare(password,user.password)
-     if (confirm){
+module.exports.getPass=async(req,res)=>{     
+     try{
+        const emailId=req.body.emailId
+        const password=req.body.password
+        if (!emailId | !password){
+            res.status(400).render('notifyError',{response:"Fields are Missing",message1:"Some of your fields are missing please go back to retry.",message2:"If you want you can also go back to homepage.",button1:"Back",button2:"Home Page"})
+        }else{
+         const user=await User.findOne({emailId:emailId})
+         const confirm=await bcrypt.compare(password,user.password)
+        if (confirm){
          const passwords=await Password.find({userId:user._id}).select("-__v -userId")
          res.status(200).render('mainPage',{passwords:passwords})
-        //  res.status(200).json({success:"true",length:passwords.length,result:passwords})
-     } 
+        }else{
+            res.status(400).render('notifyError',{response:"Invalid password or Email",message1:"Please go back and enter the right password and email.",message2:"If you don't have account then go to the homepage and signup .",button1:"Retry",button2:"Home Page"})
+        }
+        }
+    }catch(err){
+        res.status(400).render('notifyError',{response:"Invalid password or Email",message1:"Please go back and enter the right password and email.",message2:"If you don't have account then go to the homepage and signup .",button1:"Retry",button2:"Home Page"})
+     }
     }
+
 module.exports.addPass=async(req,res)=>{
     const emailId=req.body.emailId
     const key=req.body.key
     const value=req.body.value
     const user=await User.find({emailId:emailId})
-    if (!user[0]) res.status(400).json({success:"false",description:"email dosen't exists"})
-    else{
-    const password= new Password({
+    if (!user[0]){
+        res.status(400).render('notifyError',{response:"User Dosen't exists",message1:"Please go back and enter the right Email.",message2:"If you don't have account then go  to the homepage and signup .",button1:"Retry",button2:"Home Page"})
+    }else{
+        const password= new Password({
         userId:user[0]._id,
         key:key,
         value:value
